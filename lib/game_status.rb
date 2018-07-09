a327e9754b927c7142c4b6f1db7dfe9067bc5983
harrison tran <harrisontran@outlook.com>
2018-07-09 21:50:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,3 +16,21 @@ WIN_COMBINATIONS = [
 [0,4,8], #Diagnonals
 [2,4,6],
 ]
+def won(board)
+    WIN_COMBINATIONS.each do |index_array|
+      index_array.each do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = board[win_index_1] # load the value of the board at win_index_1
+        position_2 = board[win_index_2] # load the value of the board at win_index_2
+        position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+        if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O" 
+          return win_combination
+        else
+          false  
+        end
+      end
+end
