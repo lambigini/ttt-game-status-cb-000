c6b75b4cb2402668af834ce8a0fa0a72e390dbee
harrison tran <harrisontran@outlook.com>
2018-07-09 22:31:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,13 @@ WIN_COMBINATIONS = [
 [2,4,6]
 ]
 def won?(board)
-    WIN_COMBINATIONS.detect do |combo|
-      board[combo[0]] == board[combo[1]] && board [combo[1]] == board[combo[2]] &&
-      position_taken?(board,combo[0])
-end
+    WIN_COMBINATIONS.each do |win_combination|
+      win_combination.each do |win_index|
+        if board[win_index] = "X"
+          return win_combination
+        else
+          return false
+        
+      end
+    end
 end
